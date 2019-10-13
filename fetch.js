dabc2e92f8b5640c8dc108f45e039f4741e3121f
Julien Chigot <you@example.com>
2019-10-13 23:07:41 +0200

    Enter

--- fetch.js
+++ fetch.js
@@ -1,45 +1,16 @@
+function getPokemon() {
+    var p = document.getElementById("choix").value; 
+console.log(p);
+}   
 fetch('https://pokeapi.co/api/v2/pokemon/')
-.then(response => response.json())
+.then(reponse => reponse.json())
 .then (json => {
-    let ul = document.querySelector('#pokemons')
+    let ul = document.querySelector('#pokemons');
     json.results.forEach(pokemon => {
-    let li = document.createElement('li')
-    let txt = document.createTextNode(pokemon.name)
-    li.appendChild
-    ul.appendChild(li)
-
-    fillPokemonDetail(pokemon)
-    fetch('https://pokeapi.co/api/v2/stat/')
-    .then(content => li.appendChild(content))
-
+        let li = document.createElement('li')
+        let txt = document.createTextNode(pokemon.name)
+        li.appendChild
+        ul.appendChild(li)
     })
-    })
-
-function fillPokemonDetail(pokemon) {
-    return fetch(pokemon.url)
-    .then(response => response.json())
-    .then(details => {
-        let stats = details.stats
-        let div = document.createElement('div')
-    div.textContent
-        return div
-
-    })
-}
-
-
-
-
-
-
-
-fetch(//URL API//)
-.then(response => response.json)
-.then(result => {// CHOISI PAR DEV/
-    let manga = result.data.attatributes
-let h1 = document.querySelector('[data-title]') // SELECTIONNE DANS LE HTML data-title
-    h1.appendChild(document.createTextNode(manga.titles.en)) // AJOUTE DANS DATA TITLE
-let nextRelease = moment(manga.nextRelease).format('DD/MM/YYYY')
-let release = document.querySelector('[data-release]')
-release.appendChild(document.createTextNode(nextRelease))
 })
+

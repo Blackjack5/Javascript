4dca80cdfab243c734f37097529db274e7715e94
Julien Chigot <you@example.com>
2019-11-15 14:50:05 +0100

    test

--- fetch.js
+++ fetch.js
@@ -1,16 +1,15 @@
 var url = "https://pokeapi.co/api/v2/";
 function getPokemon() {
-    p = document.getElementById("choix").value;
-    getPokemon1(p); 
-} 
-function getPokemon1(p) {
-    return p;
+  p = document.getElementById("choix").value;
+  getPokemon1(p);
 }
-fetch(url + "pokemon/"+ "butterfree")
-.then(response => response.json())
-.catch(error => alert("Erreur : Oups un problème est survenu avec notre API"))
-.then(response => console.log(JSON.stringify(response)))
-.then(function(data) {
-    let pokemon = data.results
-    console.log(data)
-})
+function getPokemon1(p) {
+  return p;
+}
+fetch(url + "pokemon/" + "butterfree")
+  .then(response => response.json())
+  .catch(error => alert("Erreur : Oups un problème est survenu avec notre API"))
+  .then(response => console.log(JSON.stringify(response)))
+  .then(function(data) {
+    let pokemon = data.results;
+  });
